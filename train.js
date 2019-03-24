08c6c2a61f832268c42a96c2406b49ed8a484f2c
nikki.zand <nzand54@gmail.com>
2019-03-23 20:39:28 -0400

    final

---
+++ train.js
@@ -0,0 +1,76 @@
+// THE SOURCE OF MOST OF THIS CODE CAME FROM THE ACTIVITIES IN CLASS
+
+var config = {
+  apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+  authDomain: "time-sheet-55009.firebaseapp.com",
+  databaseURL: "https://nikza-54693.firebaseio.com/",
+  storageBucket: "time-sheet-55009.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainStart = $("#start-input").val().trim();
+  var trainFrequency = $("#frequency-input").val().trim();
+
+  // Creates  "temporary" object 
+  var newtrain = {
+    name: trainName,
+    destination: trainDestination,
+    start: trainStart,
+    frequency: trainFrequency
+  };
+
+  // Uploads train data   
+  database.ref().push(newtrain);
+
+  alert("train successfully added");
+
+  // Clear all
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#start-input").val("");
+  $("#frequency-input").val("");
+});
+
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainStart = childSnapshot.val().start;
+  var trainFrequency = childSnapshot.val().frequency;
+
+    var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+
+    var currentTime = moment();
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    var tRemainder = diffTime % trainFrequency;
+
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+    var nextTrain2 = moment(nextTrain).format("hh:mm")
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDestination),
+    $("<td>").text(nextTrain2),
+    $("<td>").text(trainFrequency),
+    $("<td>").text(tMinutesTillTrain),
+  );
+
+  $("#train-table > tbody").append(newRow);
+});
+
